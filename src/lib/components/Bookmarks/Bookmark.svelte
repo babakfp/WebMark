53d9a8fbb5a48afcef23c85c029e6273bbee0f69
babakfp <babak.bxf@gmail.com>
2025-04-04 02:24:38 +0330

    add glob emoji for no icon bookmarks

--- src/lib/components/Bookmarks/Bookmark.svelte
+++ src/lib/components/Bookmarks/Bookmark.svelte
@@ -11,12 +11,14 @@
 <div class="flex h-12 items-center gap-2 border-2 border-gray-800 px-4">
     {#if bookmark.type === "folder"}
         📁
-    {:else}
+    {:else if bookmark.icon}
         <img
             class="aspect-square h-4 w-4"
             src={bookmark.icon}
             alt={bookmark.title}
         />
+    {:else}
+        🌐
     {/if}
     <span class="line-clamp-1">{bookmark.title}</span>
 </div>
